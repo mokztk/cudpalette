756e561e3b688d9075e4b3be870ac968f4a9426d
MORI Kazutaka @Rstudio Server <mori.kazutaka@gmail.com>
2023-10-20 15:32:22 +0900

    new function jis_color() for JIS Z 9103:2018 color codes.

--- R/jis_color.R
+++ R/jis_color.R
@@ -1,23 +1,20 @@
-#' Get CUD color code
-#'
-#' Get Color Universal Design or JIS Z 9103:2018 color code(s) from color name(s).
-#' The color name(s) for JIS color should be specified with a "jis_" prefix.
+#' Get JIS Z 9103:2018 color code
 #'
 #' @param colname The vector of color names to convert to hexadecimal color codes.
 #' @return vector of hexadecimal color codes (#rrggbb).
 #' @family color
 #' @export
 #'
-cud_color <- function(colname) {
+jis_color <- function(colname) {
   res <- sapply(colname,
                 function(x) {
-                  n <- match(tolower(x), names(list_all_cols))
+                  n <- match(tolower(x), names(list_jis_cols))
                   # if specified color is not exist, return NA with alert
                   if (is.na(n)) {
                     message(paste("undefined color name :", x))
                     return(NA_character_)
                   } else {
-                    unlist(list_all_cols[x])
+                    unlist(list_jis_cols[x])
                   }
                 })
 
